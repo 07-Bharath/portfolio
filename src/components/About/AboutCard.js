4f950dbf48965abeedcb812d0236b23d7a3db6b2
BHARATH RAJ <155564304+07-Bharath@users.noreply.github.com>
2024-05-22 09:46:12 +0530

    Add files via upload

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">BHARATH RAJ </span>
+            from <span className="purple"> CHENNAI.</span>
+            <br />
+            I am currently enrolled at Velammal College, where I am pursuing a degree in Computer Science and Engineering.
+            <br />
+            This field captivates me due to its dynamic nature and the boundless opportunities it offers for innovation and problem-solving.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer"></footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
